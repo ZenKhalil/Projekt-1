1b4a3578104327fbdc19a135318208ea1c38ca70
ZenKhalil <feattzen@gmail.com>
2023-01-31 18:03:49 +0100

    New changes incl. age and mail info

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ document.body.innerHTML += "Hello JavaScript!";
 console.log("Hello Dat.js");
 console.log("Hi Students!");
 console.log("This is your first JavaScript App 🎉");
+
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hello " +
+    document.querySelector("#name").value +
+    ". You are " +
+    document.querySelector("#age").value +
+    " years old. Your mail is: " +
+    document.querySelector("#mail").value;
+}
+
+document.querySelector("button").addEventListener("click", sayHello);
